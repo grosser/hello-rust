641ab4817e87fb2a83450439dee8824518e1b4cb
Michael Grosser <michael@grosser.it>
2016-04-03 14:44:07 -0700

    monsters

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,41 @@
 #![feature(plugin)]
 #![cfg_attr(test, plugin(stainless))]
 
+pub struct Monster {
+  health: usize
+}
+
+impl Monster {
+  pub fn new(health: usize) -> Monster {
+    Monster {
+      health: health
+    }
+  }
+
+  pub fn healthy(&self) -> bool {
+    if self.health >= 50 {
+      true
+    } else {
+      false
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
-	pub use std::collections::HashMap;
+  pub use super::Monster;
 
-	describe! stainless {
-		it "can use HashMap" {
-			let mut map = HashMap::new();
-			map.insert(1, 1);
-			assert!(map.contains_key(&1));
-		}
-	}
+  describe! monster {
+    describe! healthy {
+      it "is healthy when above 50" {
+        let monster = Monster::new(50);
+        assert_eq!(true, monster.healthy());
+      }
+
+      it "is unhealthy when below 50" {
+        let monster = Monster::new(49);
+        assert_eq!(false, monster.healthy());
+      }
+    }
+  }
 }
