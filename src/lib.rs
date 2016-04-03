d7b033acda2bf561652cf31ab2df330197706e83
Michael Grosser <michael@grosser.it>
2016-04-03 15:13:25 -0700

    friendly

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,15 @@
 #![cfg_attr(test, plugin(stainless))]
 
 pub struct Monster {
-  health: usize
+  health: usize,
+  eyes: usize
 }
 
 impl Monster {
-  pub fn new(health: usize) -> Monster {
+  pub fn new(health: usize, eyes: usize) -> Monster {
     Monster {
-      health: health
+      health: health,
+      eyes: eyes
     }
   }
 
@@ -17,6 +19,14 @@ impl Monster {
       true
     } else {
       false
+    }
+  }
+
+  pub fn friendly(&self) -> &str {
+    match self.eyes {
+      1 => "evil",
+      2 => "normal",
+      _ => "cute"
     }
   }
 }
@@ -28,13 +38,30 @@ mod tests {
   describe! monster {
     describe! healthy {
       it "is healthy when above 50" {
-        let monster = Monster::new(50);
+        let monster = Monster::new(50, 1);
         assert_eq!(true, monster.healthy());
       }
 
       it "is unhealthy when below 50" {
-        let monster = Monster::new(49);
+        let monster = Monster::new(49, 1);
         assert_eq!(false, monster.healthy());
+      }
+    }
+
+    describe! friendly {
+      it "is evil with 1 eye" {
+        let monster = Monster::new(49, 1);
+        assert_eq!("evil", monster.friendly());
+      }
+
+      it "is normal with 2 eyes" {
+        let monster = Monster::new(49, 2);
+        assert_eq!("normal", monster.friendly());
+      }
+
+      it "is cute with 3 eyes" {
+        let monster = Monster::new(49, 3);
+        assert_eq!("cute", monster.friendly());
       }
     }
   }
